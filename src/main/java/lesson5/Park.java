21c687a419bda21939f75bde18020377930bbd0c
Fati <fatigeass@gmail.com>
2025-03-06 19:12:55 +0300

    lesson5

---
+++ src/main/java/lesson5/Park.java
@@ -0,0 +1,30 @@
+package lesson5;
+
+public class Park {
+    String name;
+    String location;
+
+    // Внутренний класс для аттракционов
+    class Attraction {
+        String attractionName;
+        String workingHours;
+        int price;
+
+        public Attraction(String attractionName, String workingHours, int price) {
+            this.attractionName = attractionName;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+
+        public void printInfo() {
+            System.out.println("Аттракцион" + attractionName);
+            System.out.println( "Часы работы: " + workingHours);
+            System.out.println("Цена: " + price + "$");
+        }
+    }
+
+    public Park(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+}
